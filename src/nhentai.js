2f92235a27309991b1cd22eb481fec94691f6ac6
0eti <samuelzhang012@gmail.com>
2023-02-14 20:43:38 -0800

    gallery url extractor operational

--- src/nhentai.js
+++ src/nhentai.js
@@ -1,39 +1,44 @@
 const getDoujinPageCount = async () => {
   const tagsElementText = document.getElementById("tags").outerText;
-  console.log(tagsElementText);
-  // const match = tagsElementText.matchAll(/Pages:\s*([0-9]+)\s*Uploaded/g);
   const match = [
     ...tagsElementText.matchAll(/Pages:\s*([0-9]+)\s*Uploaded/g),
   ][0];
-  // console.log(matches);
-  // for (match of matches) {
-  //   console.log("SANITY CHECK");
-  //   console.log(match);
-  // }
-
-  // console.log(match);
-  return match;
+  return Number(match[1]);
 };
 
-const getGalleryURLs = async () => {
+const getGalleryURLComponents = async () => {
   const firstPageHTML = await fetch(`${window.location.href}/1/`, {
     method: "GET",
   }).then((res) => res.text());
 
   const match = [
     ...firstPageHTML.matchAll(
-      /<img src="(https:\/\/\S*nhentai\.net\/galleries)\/([0-9]+)\/([0-9]+)(\.\S+)" width="[0-9]+" height="[0-9]+" \/>/g
+      /<img src="(https:\/\/\S*nhentai\.net\/galleries)\/([0-9]+)\/([0-9]+)\.(\S+)" width="[0-9]+" height="[0-9]+" \/>/g
     ),
   ][0];
 
-  return match;
+  return {
+    urlBase: match[1],
+    galleryID: match[2],
+    fileType: match[4],
+  };
 };
 
-const getDoujinImageURLs = async () => {
-  const galleryURLs = await getGalleryURLs();
+const generateGalleryURLs = (pageCount, galleryURLComponents) => {
+  const galleryURLs = [];
+  for (var index = 1; index <= pageCount; index++) {
+    galleryURLs.push(
+      `${galleryURLComponents.urlBase}/${galleryURLComponents.galleryID}/${index}.${galleryURLComponents.fileType}`
+    );
+  }
+  return galleryURLs;
+};
+
+const exuOnClickHandler = async () => {
+  const pageCount = await getDoujinPageCount();
+  const galleryURLComponents = await getGalleryURLComponents();
+  const galleryURLs = generateGalleryURLs(pageCount, galleryURLComponents);
   console.log(galleryURLs);
-  const doujinPageCount = await getDoujinPageCount();
-  console.log(doujinPageCount);
 };
 
 const main = async () => {
@@ -48,7 +53,7 @@ const main = async () => {
     document.body.innerHTML += `<img id="exu" style="position:fixed; top:10px; right:10px; border-radius:10px; cursor:pointer;" width="64" height="64" src='${exuDataURL}'>`;
 
     const exu = document.getElementById("exu");
-    exu.addEventListener("click", getDoujinImageURLs);
+    exu.addEventListener("click", exuOnClickHandler);
   }
 };
 
