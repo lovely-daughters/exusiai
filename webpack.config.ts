3a01cd20c998b1ae900636dcb728a8ba0162ac70
0eti <samuelzhang012@gmail.com>
2023-02-18 21:49:09 -0800

    saving before webpack test

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,2 +1,23 @@
 import * as path from "path";
 import * as webpack from "webpack";
+
+const config: webpack.Configuration = {
+  entry: {
+    background: path.join(__dirname, "src", "background.ts"),
+    nhentai: path.join(__dirname, "src", "nhentai.ts"),
+  },
+  output: {
+    filename: path.join(__dirname, "dist", "[name].js"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: "tsc",
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
+
+export default config;
